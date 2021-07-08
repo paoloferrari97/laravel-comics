7ce5bb0ff009f6e048a4069de80576c730e8146f
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-08 21:01:17 +0200

    Finisco l'ultima parte del main di comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -23,5 +23,30 @@
         </div>
 
     </div>
+
+    <div class="barra_blu">
+        <div class="container">
+            <div>
+                <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div>
+                <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+                <span>DC MERCHANDISE</span>
+            </div>
+            <div>
+                <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+                <span>SUBSCRIPTION</span>
+            </div>
+            <div>
+                <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+                <span>COMIC SHOP LOCATOR</span>
+            </div>
+            <div>
+                <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
