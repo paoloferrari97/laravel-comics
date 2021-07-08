a744b00f13875dff277977e670ad95cca1dfde70
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-08 19:30:34 +0200

    Inizio a creare il main della pagina comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.app')
 
 @section('main')
-    
+<div class="comics">
+    <div class="container">
+        <div class="current_series">CURRENT SERIES</div>
+    </div>
+</div>
 @endsection
