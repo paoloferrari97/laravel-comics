1c33dbcf6317474ea5bda9682b96b32f797e5623
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-08 20:37:45 +0200

    Uso foreach per mostrare i comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,6 +4,20 @@
 <div class="comics">
     <div class="container">
         <div class="current_series">CURRENT SERIES</div>
+
+        <div class="cards">
+            @foreach ($comics as $comic)
+            <div class="card">
+                <div>
+                    <img src="{{$comic['thumb']}}" alt="Copertina Comic">
+                </div>
+                <div>
+                    {{$comic['title']}}
+                </div>
+            </div>    
+            @endforeach
+        </div>
+
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
+    $comics = config('comics.data');
+    $data = [
+        'comics' => $comics
+    ];
+
+    return view('comics', $data);
 })->name('Comics');
