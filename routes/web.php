88ed74e2fc831ca2c59330bab3572a131bd26674
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-09 10:25:24 +0200

    Creo le altre pagine estendendo il layout standard

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,48 @@ Route::get('/', function () {
 
     return view('comics', $data);
 })->name('Comics');
+
+Route::get('/characters', function () {
+
+    return view('characters');
+})->name('Characters');
+
+Route::get('/movies', function () {
+
+    return view('movies');
+})->name('Movies');
+
+Route::get('/tv', function () {
+
+    return view('tv');
+})->name('Tv');
+
+Route::get('/games', function () {
+
+    return view('games');
+})->name('Games');
+
+Route::get('/collectibles', function () {
+
+    return view('collectibles');
+})->name('Collectibles');
+
+Route::get('/videos', function () {
+
+    return view('videos');
+})->name('Videos');
+
+Route::get('/fans', function () {
+
+    return view('fans');
+})->name('Fans');
+
+Route::get('/news', function () {
+
+    return view('news');
+})->name('News');
+
+Route::get('/shop', function () {
+
+    return view('shop');
+})->name('Shop');
