1c95bcfd820630cca89d98fd3ff31b67ce798593
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-09 15:43:42 +0200

    Sistemo img di ogni singolo comic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,1 +1,15 @@
 @extends('layout.app')
+
+@section('main')
+
+<div class="sez_blu_comic">
+    <div class="container2">
+        <div>
+            <img src="{{$comic['thumb']}}" alt="">
+            <div class="comic_book">COMIC BOOK</div>
+            <div class="view_gallery">VIEW GALLERY</div>
+        </div>        
+    </div>
+</div>
+
+@endsection
